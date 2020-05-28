360eaa67e935b6761eed10c9f6d45af09880517b
Vini Salazar <viniws@gmail.com>
2020-05-27 22:30:10 -0300

    Add NCBI metadata tables and download commands

---
+++ data/ncbi/cyano_download.sh
@@ -0,0 +1,1 @@
+ncbi-genome-download -s genbank -F all -p 5 -r 5 -m cyano_complete_references.csv -A all_cyanobacteria.txt -v bacteria
---
+++ data/ncbi/synecho_download.sh
@@ -0,0 +1,1 @@
+ncbi-genome-download -s genbank -F fasta,assembly-report -g "Synechococcus" --fuzzy-genus -m synechoccus_ncbi_metadata_27-05.tsv bacteria
